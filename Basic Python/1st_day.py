ca716e77d330af2cdcd91eec9e87a24f79aab79a
Md-Sakib-Hossain-Shovon <sakib.aiubedu.cse@gmail.com>
2022-03-22 18:39:00 +0600

    Basic Python Code Uploaded

---
+++ Basic Python/1st_day.py
@@ -0,0 +1,67 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2+2
+4
+>>> 2**2
+4
+>>> 7%2
+1
+>>> 21//2
+10
+>>> 21/2
+10.5
+>>> 3*5
+15
+>>> 3-3
+0
+>>> 3+6
+9
+>>> (5-1)*((7+1)/(3-1))
+16.0
+>>> 'sakib'+'hossen'
+'sakibhossen'
+>>> 'sakib'*3
+'sakibsakibsakib'
+>>> abc=40
+>>> abc
+40
+>>> dfg=35
+>>> dfg
+35
+>>> abc+dfg
+75
+>>> xyz='hello'
+>>> hhh='bye'
+>>> xyz+hhh
+'hellobye'
+>>> myname=input()
+print('My Name Is,'+myname)
+>>> sakib
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    sakib
+NameError: name 'sakib' is not defined
+>>> myname
+"print('My Name Is,'+myname)"
+>>> myname='sakib'
+>>> print('Name is'+myname)
+Name issakib
+>>> print(len(myname))
+5
+>>> print('what is your age?')
+what is your age?
+>>> myage=input()
+print('Age will be'+str(int(myage)+1))
+>>> 56
+56
+>>> myage
+"print('Age will be'+str(int(myage)+1))"
+>>> myage=input()
+5
+>>> print('Age will be'+str(int(myage))
+5
+      
+SyntaxError: invalid syntax
+>>> print
+<built-in function print>
+>>> 
